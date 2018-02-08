591e522ed42b721416b28aff68061ac3d2799cfa
Samantha Ngo <sngo1@stuy.edu>
2018-02-08 09:08:54 -0500

    Growing circle working. Trying to figure out how to make it stop.

--- grow.js
+++ grow.js
@@ -10,45 +10,105 @@ console.log("canvas: ", canvas);
 // Set the context for canvas actions
 ctx = canvas.getContext("2d");
 
-var lastX = 50;
-var lastY = 50;
+var x = 300;
+var y = 300;
+var requestID;
+var requested = false;
+var radius = 10;
+var grow = true;
 
 // Given the coordinates relative to the canvas upper left corner, draw a circle
 // at that location.
-var drawCircle = function(x,y){
+var drawCircle = function(){
     console.log("X: ", x);
     console.log("Y: ", y);
 
     ctx.beginPath();
-    ctx.arc(x, y, 20, 0, 2 * Math.PI);
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
     ctx.fillStyle = "hotpink"
     ctx.fill()
     console.log("Drew a circle.")
-    while (lastX < 1000){
-	lastX += 1;
-	    clearCanvas();
-	console.log(window.requestAnimationFrame(drawCircle(lastX, lastY)));
-    }
     return true
 }
+
+var stop = function(){
+    window.cancelAnimationFrame(requestID);
+    return true;
+}
+
+var animate = function(){
+    clearCanvas();
+    console.log("Radius: ", radius);
+    drawCircle();
+
+    if (radius == 200){
+	grow = false;
+    } else if(radius == 5){
+	grow = true;
+    }
+    
+    if (grow){
+	radius += 5;
+    } else {
+	radius -= 5;
+    } 
+	
+    requestID = window.requestAnimationFrame(animate);
+    console.log("REQID# ", requestID);
+    return true;
+}
+
+/*
+var direction = "right";
 
 // Based on the global state variable, decide whether to draw a circle or a
 // square, connect the new point to the last point, and then draw the shape.
 var animate = function(){
-    window.requestAnimationFrame(drawCircle(lastX, lastY));
-    return true;
-}
+    clearCanvas();
+    drawCircle(lastX, lastY);
+   
+    if (direction == "right"){
+	console.log("Heading ", direction);
+	if (lastX < 550){
+	    lastX += 5
+	} else {
+	    direction = "down";
+	}
+    } else if (direction == "down"){
+	console.log("Heading ", direction);
+	if (lastY < 550){
+	    lastY += 5;
+	} else {
+	    direction = "left";
+	}
+    } else if (direction == "left"){
+	console.log("Heading ", direction);
+	if (lastX > 50){
+	    lastX -= 5;
+	} else {
+	    direction = "up";
+	}
+    } else if (direction == "up"){
+	console.log("Heading ", direction);
+	if (lastY > 50){
+	    lastY -= 5;
+	} else {
+	    direction = "right";
+	}
+    } else {
+	stop();
+	console.log("Animation finished.");
+    }
 
-var stop = function(){
-    window.cancelAnimationFrame(lastX);
+    requestID = window.requestAnimationFrame(animate);
+    console.log("REQID# ", requestID);
     return true;
 }
+*/
 
 // Draw a white rectangle over the canvas to clear it.
 var clearCanvas = function(){
     ctx.beginPath();
-    lastX = -1;
-    lastY = -1;
     ctx.fillStyle = "#ffffff";
     ctx.fillRect(0,0,600,600);
     console.log("Canvas cleared.");
@@ -58,8 +118,9 @@ var clearCanvas = function(){
 // Listen for user clicks
 canvas.addEventListener('click', animate);
 
-animate();
-
 var stop = document.getElementById("stop");
 console.log("stop: ", stop);
-stop.addEventListener('click', changeShape);
+stop.addEventListener('click', stop);
+var start = document.getElementById("start");
+console.log("start: ", start);
+start.addEventListener('click', animate);
